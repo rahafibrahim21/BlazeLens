b378548503160a8ab18c93b596d91a63d7466256
rahafibrahim21 <rahafibrahim999@gmail.com>
2024-05-24 21:55:07 +0300

    Noti done..

---
+++ BlazeLens/View/UserNotifications.swift
@@ -0,0 +1,18 @@
+//
+//  UserNotifications.swift
+//  BlazeLens
+//
+//  Created by Rahaf ALghuraibi on 16/11/1445 AH.
+//
+
+import SwiftUI
+
+struct UserNotifications: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserNotifications()
+}
